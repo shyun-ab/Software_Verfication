ce832c6f0ca95b620adba05c4ed4ed305b477375
shyun-ab <shyun.ab@gmail.com>
2018-05-28 00:16:39 +0900

    프로젝트 폴더 구성 변경

---
+++ Test/src/DataBase/BankTest.java
@@ -0,0 +1,22 @@
+package DataBase;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankTest {
+
+	@Test //아이디 제대로 입력
+	   public void searchIDtest1() {
+	      //fail("Not yet implemented");
+	      Bank test = new Bank(1);
+	      assertEquals(1,test.searchID(325,1));
+	   }
+	   
+	   @Test //아이디 다르게 입력
+	   public void searchIDtest2() {
+	      Bank test = new Bank(1);
+	      assertEquals(0,test.searchID(324, 1));
+	   }
+
+}
